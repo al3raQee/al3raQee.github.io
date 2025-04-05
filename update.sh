fb003d6c3ab8e87dcc9c2cc7852e23fba2ce8348
al3raQi <37264883+al3raQee@users.noreply.github.com>
2025-04-05 02:32:18 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,6 @@
 set -eo pipefail
 rm -f Packages.bz2
 bzip2 -c9 Packages > Packages.bz2
-rm -f Packages.gz
 gzip -c9 Packages > Packages.gz
 git add debs Packages.bz2 depictions
 git commit -m "update"
