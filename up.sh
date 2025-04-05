c7e75487f91fd750c720a8df9e1b00d4ad5ddcd0
Mobile User <amir_aliraq@hotmail.com>
2025-04-05 03:03:43 +0200

    update

--- up.sh
+++ up.sh
@@ -1,8 +1,3 @@
-rm -f Packages.bz2
-rm -f Packages.gz
-bzip2 -c9 Packages > Packages.bz2
-gzip -c9 Packages > Packages.gz
-git add debs Packages.bz2 depictions
-git add debs Packages.gz depictions
+git add --all
 git commit -m "update"
 git push
